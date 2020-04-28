d9d7944a8671753c44b75bbe93c7fc9e61ab2a57
thedekerone <mauriciofow@gmail.com>
2020-04-28 15:15:58 -0500

    finished my_each and my_each_with_index methods

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,23 @@
 module Enumerable
-    def my_each(array)
-      for i in 0...array.length
-        p "gaa"
+    def my_each
+        for i in 0...length
+            yield self[i]
+        end
+    end
+
+    def my_each_with_index
+        for i in 0...length
+            yield self[i],i
+        end
     end
 end
+
+testArr=%w(5 6 7)
+
+p "My each test"
+
+testArr.my_each{|number| p number}
+
+p "My each with index test"
+
+testArr.my_each_with_index{|number,index| p "#{number} has index of #{index}"}
