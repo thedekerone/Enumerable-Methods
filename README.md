c0a0074041d6b98fee5c9576b164fb0c8fc86588
thedekerone <mauriciofow@gmail.com>
2020-04-29 14:51:17 -0500

    fixed readme and rubocop errors

---
+++ README.md
@@ -0,0 +1,29 @@
+# Methods project
+
+This is a project of two algorithms about Enumerble methods in ruby.
+
+The methods in this code are enumerables to work with arrays.
+
+it contains copies of all the most used enumerables.
+
+## Built With
+
+- Ruby
+
+## Authors
+
+👤 **Mauricio Fow**
+
+- Github: [@githubhandle](https://github.com/thedekerone)
+- Twitter: [@twitterhandle](https://twitter.com/mauricio_fow)
+- Linkedin: [linkedin](https://www.linkedin.com/in/mauricio-fow-aranibar-b2173514b/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
--- my_each.rb
+++ my_each.rb
@@ -1,150 +1,143 @@
 module Enumerable
-    def my_each
-        for i in 0...length
-            yield self[i]
-        end
-        self
+  def my_each
+    (0...length).each do |i|
+      yield self[i]
     end
+    self
+  end
 
-    def my_each_with_index
-        for i in 0...length
-            yield self[i],i
-        end
-        self
+  def my_each_with_index
+    (0...length).each do |i|
+      yield self[i], i
     end
+    self
+  end
 
-    def my_select
-        new_array=[]
-        my_each do |element| 
-            if(yield element)
-                new_array.push(element)
-            end
-        end
-        new_array
+  def my_select
+    new_array = []
+    my_each do |element|
+      new_array.push(element) if yield element
     end
+    new_array
+  end
 
-    def my_all?
+  def my_all?
+    result = true
+    my_each do |element|
+      statement = if block_given?
+                    yield element
+                  else
+                    element
+                  end
+
+      unless statement
+        result = false
+        break
+      end
+
+      result = true
+    end
+    result
+  end
+
+  def my_any?
+    result = true
+    my_each do |element|
+      statement = if block_given?
+                    yield element
+                  else
+                    element
+                  end
+
+      if statement
         result = true
-        my_each do |element| 
-            if block_given?
-                statement=yield element
-            else
-                statement=element
-            end
-             
-            if(!statement)
-                result=false
-                break;
-            end
-            
-            result= true
-        end
-        result
+        break
+      end
+      result = false
     end
+    result
+  end
 
-    def my_any?
-        result = true
-        my_each do |element| 
-            if block_given?
-                statement=yield element
-            else
-                statement=element
-            end
-             
-            if(statement)
-                result=true
-                break;
-            end
-            result= false
-        end
-        result
+  def my_none?
+    !my_any? do |element|
+      yield element
     end
+  end
 
-    def my_none?
-        !my_any? do |element|
-            yield element
-        end
+  def my_count
+    counter = 0
+    my_each do |element|
+      param = yield element
+      counter += 1 if param.eql?(true) || param.eql?(element)
     end
+    counter
+  end
 
-    def my_count
-        counter=0
-        my_each do |element|
-            param = yield element
-            if(param.eql?(true) || param.eql?(element))
-                counter+=1
-            end
-        end
-        counter
+  def my_map(proc = nil)
+    new_arr = []
+    my_each do |element|
+      param = if proc
+                proc.call(element)
+              else
+                yield element
+              end
+      new_arr.push(param)
     end
+    new_arr
+  end
 
-    def my_map(proc=nil)
-        new_arr=[]
-        my_each do |element|
-            if (proc)
-                param = proc.call(element)
-            else
-                param = yield element
-            end
-            new_arr.push(param)
-        end
-        new_arr
+  def my_inject(initial_value = 0)
+    acumulator = initial_value
+    my_each do |element|
+      result = yield element, acumulator
+      acumulator = result
     end
+    acumulator
+  end
 
-    def my_inject(initial_value=0)
-        acumulator=initial_value
-        my_each do |element|
-            result = yield element, acumulator
-            acumulator= result
-        end
-        acumulator
+  def multiply_els
+    my_inject(1) do |acumulator, element|
+      acumulator * element
     end
-
-    def multiply_els
-        my_inject(1) do |acumulator, element|
-            acumulator*element
-        end
-    end
-
+  end
 end
 
-testArr=[5,6,7]
+# TEST USING THE FOLLOWING CODE
 
-# p "My each test"
+# test_arr = [5, 6, 7]
 
-# p testArr.my_each{|test|  test}
+# p 'My each test'
 
-# p "My each with index test"
+# test_arr.my_each { |test| p test + 3 }
 
-# testArr.my_each_with_index{|number,index| p "#{number} has index of #{index}"}
+# p 'My each with index test'
 
-# p "My select test"
+# test_arr.my_each_with_index { |number, index| p "#{number} has index of #{index}" }
 
-# p testArr.my_select{|number| number!=(6)}
+# p 'My select test'
 
-# p "My all test"
+# p test_arr.my_select { |number| number != 6 }
 
-# p testArr.my_all?{|number| number>3}
+# p 'My all test'
 
-# p "My any test"
+# p test_arr.my_all? { |number| number > 3 }
 
-# p testArr.my_any?{|number| number}
+# p 'My any test'
 
-# p "My none test"
+# p test_arr.my_any? { |number| number }
 
-# p testArr.my_none?{|number| number>7}
+# p 'My none test'
 
-# p "My count test"
+# p test_arr.my_none? { |number| number > 7 }
 
-# p testArr.my_count{|number| number>5}
+# p 'My count test'
 
-# p "My map test"
-# proc= Proc.new {|element| element}
-# p testArr.my_map(proc){|number| number+5}
+# p test_arr.my_count { |number| number > 5 }
 
-# p "My inject test"
+# p 'My map test'
+# proc = proc { |element| element }
+# p test_arr.my_map(proc) { |number| number + 5 }
 
-# p testArr.multiply_els
+# p 'My inject test'
 
-# t = Proc.new {|element| element}
-
-# p testArr.my_map{|number| t.call(number)}
+# p test_arr.multiply_els
