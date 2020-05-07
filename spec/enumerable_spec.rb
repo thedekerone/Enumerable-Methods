23cfe7782071c5c8d05e7da7edad9ffc65341467
shubhsk88 <shubham88singh@gmail.com>
2020-05-08 00:55:00 +0530

    Fixing the my_all testing

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -73,22 +73,32 @@ describe Enumerable do
       expect { wrong_type.my_select }.to raise_error(NoMethodError)
     end
   end
-  describe "#my_all?" do
-    context "When the block is given" do
-        it "every element matches the condition in block" do
-        expect(array.my_all?{|x| x>=0}).to eql(true)
-        end
+  describe '#my_all?' do
+    context 'When the block is given' do
+      it 'every element matches the condition in block' do
+        expect(array.my_all? { |x| x >= 0 }).to eql(true)
+      end
 
-        it "not every elements matches the condition in block" do 
-            expect(array.my_all?{|x| x>1}).to eql(false)
-        end
-        
-    end
-    
-    context "When the block is not given" do
-        
+      it 'not every elements matches the condition in block' do
+        expect(array.my_all? { |x| x > 1 }).to eql(false)
+      end
     end
 
-end
-
+    context 'When the block is not given and parameter is given:' do
+      it 'regex' do
+        expect(%w[ant bear cat].my_all?(/t/)).to eql(false)
+      end
+      it 'Class' do
+        expect([1, 2i, 3.14].my_all?(Numeric)).to eql(true)
+      end
+    end
+    context 'When the neither the block or parameter is given' do
+      it 'the array is empty' do
+        expect([].my_all?).to eql(true)
+      end
+      it 'the array is not empty' do
+        expect([nil, true, 99].my_all?).to eql(false)
+      end
+    end
+  end
 end
